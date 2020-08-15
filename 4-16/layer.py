a88b30b75e2824e0af6c287a5d19934215eb8fa9
TJUMMG <tjummg@163.com>
2020-08-15 18:35:53 +0800

    logs

---
+++ 4-16/layer.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+
+def conv_layer(x, filter_shape, stride, trainable=True):
+    filter_ = tf.get_variable(
+        name='weight', 
+        shape=filter_shape,
+        dtype=tf.float32, 
+        initializer=tf.contrib.layers.xavier_initializer(),
+        trainable=trainable)
+    return tf.nn.conv2d(
+        input=x,
+        filter=filter_,
+        strides=[1, stride, stride, 1],
+        padding='SAME')
+
+def deconv_layer(x, filter_shape, output_shape, stride, trainable=True):
+    filter_ = tf.get_variable(
+        name='weight',
+        shape=filter_shape,
+        dtype=tf.float32,
+        initializer=tf.contrib.layers.xavier_initializer(),
+        trainable=trainable)
+    return tf.nn.conv2d_transpose(
+        value=x,
+        filter=filter_,
+        output_shape=output_shape,
+        strides=[1, stride, stride, 1])
+
+def batch_normalize(x, is_training, decay=0.99, epsilon=0.001, trainable=True):
+    def bn_train():
+        batch_mean, batch_var = tf.nn.moments(x, axes=[0, 1, 2])
+        train_mean = tf.assign(
+            pop_mean, pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(
+            pop_var, pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(
+                x, batch_mean, batch_var, beta, scale, epsilon)
+
+    def bn_inference():
+        return tf.nn.batch_normalization(
+            x, pop_mean, pop_var, beta, scale, epsilon)
+
+    dim = x.get_shape().as_list()[-1]
+    beta = tf.get_variable(
+        name='beta',
+        shape=[dim],
+        dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.0),
+        trainable=trainable)
+    scale = tf.get_variable(
+        name='scale',
+        shape=[dim],
+        dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1),
+        trainable=trainable)
+    pop_mean = tf.get_variable(
+        name='pop_mean',
+        shape=[dim],
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(0.0),
+        trainable=False)
+    pop_var = tf.get_variable(
+        name='pop_var', 
+        shape=[dim],
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(1.0),
+        trainable=False)
+    return tf.cond(is_training, bn_train, bn_inference)
+
---
+++ 8-16/test_816.py
@@ -0,0 +1,88 @@
+import numpy as np
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+import cv2
+import glob
+import tensorflow as tf
+import sys
+import time
+from tqdm import tqdm
+from becalf import becalf
+
+
+def normalize(images):
+    return np.array([image/65535.0 for image in images])
+    
+
+def downscale(images):
+    #print images
+    downs = [[[[0 for p in range(3)] for k in range(1024)] for j in range(436)] for i in range(len(images))]
+    for ii in range(len(images)):
+        for j in range(len(images[ii])):
+            for k in range(len(images[ii][j])):
+                for p in range(len(images[ii][j][k])):
+                    tmp = bin(images[ii][j][k][p])
+                    tmp_quan = tmp[:-8]+'00000000'
+                    downs[ii][j][k][p] = int (tmp_quan,2)
+    downs_np = np.array(downs, dtype=np.uint16)
+    return downs_np
+    
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+config = tf.ConfigProto(allow_soft_placement=True) 
+#config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True 
+
+x = tf.placeholder(tf.float32, [None,436, 1024, 3])
+downscaled = tf.placeholder(tf.float32, [None,436, 1024, 3])
+is_training = tf.placeholder(tf.bool, [])
+
+model = becalf(x, downscaled, is_training, 1)
+sess = tf.Session(config=config)
+init = tf.global_variables_initializer()
+sess.run(init)
+
+saver = tf.train.Saver()
+saver.restore(sess, './latest')
+
+#pic = './677.png'
+pics = glob.glob('../Source50/*')
+
+tt=0
+for i in tqdm(range(len(pics))):#
+#for i in range(1):#len(pics)):#
+    x_t1 = cv2.imread(pics[i],3)
+    #x_t1 = cv2.imread(pic,3)
+    x_t1n = x_t1[np.newaxis,:,:,:]
+
+    downs = downscale(x_t1n)
+    
+    cv2.imwrite('tmp.png',downs[0])
+
+    starttime=time.time()
+    x_t1 = cv2.imread('tmp.png',3)
+    downs = x_t1[np.newaxis,:,:,:]
+    
+
+    downs_f = normalize(downs)
+    
+    fake = sess.run(model.imitation,
+        feed_dict={x: downs_f, downscaled: downs_f, is_training: False})
+
+    adda = fake + downs_f
+
+
+    full_name = pics[i].split('/')[-1]
+    #full_name = pic.split('/')[-1]
+    pure_name = full_name.split('.')[0]
+    clipped = np.clip(adda[0],0,1)
+    im = np.uint16(clipped*65535.0)
+    
+    #cv2.imwrite(pure_name+'_dsp_416.png',im)
+    cv2.imwrite('./results_816/'+pure_name+'_becalf_816.png',im)
+
+    print time.time()-starttime
+
+
+        
+        
